a4a0d68d602809f43b89333224f2eb83fb2b0794
zivchen <zivchenking@gmail.com>
2020-04-23 14:20:25 +0300

    Update index.js
    Animated.Text.propTypes.style,

--- src/components/affix/index.js
+++ src/components/affix/index.js
@@ -11,7 +11,7 @@ export default class Affix extends PureComponent {
 
   static propTypes = {
     numberOfLines: PropTypes.number,
-    style: Animated.Text.propTypes.style,
+   // style: Animated.Text.propTypes.style,
 
     color: PropTypes.string.isRequired,
     fontSize: PropTypes.number.isRequired,
